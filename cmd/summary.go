5189502b4de10069405c5cb7d15e531be38c1b92
Offpics <kornelux1@gmail.com>
2022-02-06 21:08:33 +0100

    0.004

--- cmd/summary.go
+++ cmd/summary.go
@@ -39,7 +39,7 @@ to quickly create a Cobra application.`,
 		}
 
 		for activity, time := range activities {
-			fmt.Printf("You spend %v minutes on %s \n", utils.SecondsToTime(time), activity)
+			fmt.Printf("You spent %v minutes on %s \n", utils.SecondsToTime(time), activity)
 		}
 	},
 }
