4f04339ab42e80782f68d452f70f941d0f4c8f7f
Offpics <kornelux1@gmail.com>
2022-02-08 21:12:14 +0100

    Update summary

--- cmd/summary.go
+++ cmd/summary.go
@@ -8,6 +8,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"time"
 	"tracker/types"
 	"tracker/utils"
 
@@ -28,20 +29,50 @@ to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		csvFilePath := viper.GetString("csvFilePath")
 		records := getRecordsFromCSV(csvFilePath)
-
-		activities := make(map[string]int)
-
-		for _, record := range records {
-			if _, ok := activities[record.ActivityName]; !ok {
-				activities[record.ActivityName] = 0
-			}
-			activities[record.ActivityName] += record.Time
-		}
-
-		for activity, time := range activities {
-			fmt.Printf("You spent %v minutes on %s \n", utils.SecondsToTime(time), activity)
-		}
+		printSummary(records)
 	},
+}
+
+// summaryToday represents the summary command
+var summaryToday = &cobra.Command{
+	Use:   "today",
+	Short: "A brief description of your command",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		csvFilePath := viper.GetString("csvFilePath")
+		records := getRecordsFromCSV(csvFilePath)
+		printSummary(filterRecordsForToday(records))
+	},
+}
+
+func printSummary(records []types.Record) {
+	activities := make(map[string]int)
+
+	for _, record := range records {
+		if _, ok := activities[record.ActivityName]; !ok {
+			activities[record.ActivityName] = 0
+		}
+		activities[record.ActivityName] += record.Time
+	}
+
+	for activity, time := range activities {
+		fmt.Printf("You spent %v minutes on %s \n", utils.SecondsToTime(time), activity)
+	}
+}
+
+func filterRecordsForToday(records []types.Record) []types.Record {
+	yesterday := time.Now().AddDate(0, 0, -1)
+	tomorrow := time.Now().AddDate(0, 0, 1)
+
+	filtered_records := make([]types.Record, 0)
+
+	for _, record := range records {
+		if record.Date.After(yesterday) && record.Date.Before(tomorrow) {
+			filtered_records = append(filtered_records, record)
+		}
+	}
+
+	return filtered_records
 }
 
 func getRecordsFromCSV(filePath string) []types.Record {
@@ -64,6 +95,7 @@ func getRecordsFromCSV(filePath string) []types.Record {
 
 func init() {
 	rootCmd.AddCommand(summaryCmd)
+	summaryCmd.AddCommand(summaryToday)
 
 	// Here you will define your flags and configuration settings.
 
